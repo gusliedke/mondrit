87151b4a47000356e1ba65e54d0f4e3bf16eca33
Gustavo Liedke <gustavo.liedke@lego.com>
2019-06-02 21:16:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const colours = ['black', 'white', 'yellow', 'red', 'blue'];
+
+const Square = styled.div`
+  display: block;
+  width: ${p => p.width}px;
+  height: ${p => p.height}px;
+  ${ p => p.borders ? 'border: 8px solid black;' : null }
+  background: ${p => p.color};
+`;
+
+const Frame = styled.div`
+  display: flex;
+  width: 600px;
+  height: auto;
+  margin: 0 auto;
+  flex-wrap: wrap;
+  background: white;
+`;
+
+const App = (i) => {
+  const [quantity, setQuantity] = useState(0);
+  const [border, setBorder] = useState(false);
+
+  const renderBox = () => {
+    let randomColor = Math.floor(Math.random() * 5);
+    let randomHeight = Math.floor(Math.random() * 200);
+    let randomWidth = Math.floor(Math.random() * 400);
+
+    if (randomHeight < 50 || randomWidth < 10) {
+      return null
+    }
+
+    return <Square
+        index={i}
+        color={colours[randomColor]}
+        height={randomHeight}
+        width={randomWidth}
+        borders={border}
+      >
+      </Square>
   }
+
+  const removeBorder = () => {
+    setBorder(border === false ? true : false)
+  }
+
+  return (
+    <div>
+      <div>
+        <header>
+          <p>You clicked {quantity} times</p>
+          <button onClick={() => setQuantity(quantity + 1)}>Modri it!</button>
+          <p>border {border && 'borders'}</p>
+          <button onClick={removeBorder}>Remove borders</button>
+        </header>
+        <Frame>
+          {[...Array(6)].map((e, i) => renderBox(i))}
+        </Frame>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
